3f91128b4a4ea8d0e0f82f298c5fa9f2cab1e203
Guenole Quideau <gqprof@protonmail.com>
2021-12-16 15:03:28 +0100

    Reinitialize...

---
+++ db/migrate/20211216140029_add_user_id_to_cars.rb
@@ -0,0 +1,4 @@
+class AddUserIdToCars < ActiveRecord::Migration[6.1]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_27_081252) do
+ActiveRecord::Schema.define(version: 2021_12_16_140029) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cars", force: :cascade do |t|
+    t.string "brand"
+    t.string "model"
+    t.date "year_of_production"
+    t.string "address"
+    t.integer "price_per_day"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
