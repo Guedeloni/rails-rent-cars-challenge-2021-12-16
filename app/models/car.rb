205e88a28b2469ef19e7c925ce094d3e52d5eab4
Guenole Quideau <gqprof@protonmail.com>
2021-12-16 17:30:10 +0100

    Add seed

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,10 +1,10 @@
 class Car < ApplicationRecord
   belongs_to :user
 
-  has_many :bookings, dependent: destroy
+  has_many :bookings, dependent: :destroy
 
   validates :brand, presence: true
   validates :model, presence: true
   validates :address, presence: true
-  validates :price_per_day, comparison: { greater_than: 0 }
+  validates :price_per_day, numericality: { greater_than: 0 }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,43 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts "Destroying current data..."
+User.destroy_all
+Car.destroy_all
+Booking.destroy_all
+
+puts 'Creating 10 users...'
+10.times do
+  user = User.new(
+    email: Faker::Internet.email,
+    password: Faker::Internet.password
+  )
+  user.save!
+end
+puts 'Ok for user'
+
+puts 'Creating 10 cars...'
+brands = ["Renault", "Renault", "Audi", "Audi", "Citroën", "Citroën", "Honda", "Honda", "Peugeot", "Peugeot"]
+models = ["Super 5", "Zoe", "A8", "A3", "DS4", "2CV", "Honda-T1", "Honda-T2", "403", "3008"]
+places = ["rue de la Gare, Nantes", "rue Henri IV, Nantes", "rue des Trois Croissants, Nantes",
+          "ch. de la Madeleine, Nantes", "rue de Gigant, Nantes", "rue des Tulipes, Rezé",
+          "ch. de la Cour, Ste.-Luce-sur-Loire", "bd. J. Verne, Nantes", "rue en Pierre, Nantes",
+          "av. de la Métallurgie, Carquefou"]
+users_id = []
+User.all.each do |user|
+  users_id << user.id
+end
+for i in 1..10
+  car = Car.new(
+    brand: brands[i - 1],
+    model: models[i - 1],
+    year_of_production: Faker::Vehicle.year,
+    address: places[i - 1],
+    price_per_day: (10..200).to_a.sample,
+    user_id: users_id[i - 1]
+  )
+  car.save!
+end
+puts 'Ok for cars'
