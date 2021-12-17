7c2d1a805d87c6553491512f031ceb2a7eb220d9
Guenole Quideau <gqprof@protonmail.com>
2021-12-17 12:29:59 +0100

    Add range on Flatpickr

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -1,8 +1,16 @@
 import flatpickr from "flatpickr";
+import rangePlugin from "flatpickr/dist/plugins/rangePlugin";
+
+
+// const initFlatpickr = () => {
+//   flatpickr(".datepicker", {});
+// }
 
 const initFlatpickr = () => {
-  flatpickr(".datepicker", {});
+  flatpickr("#range_start", {
+    altInput: true,
+    plugins: [new rangePlugin({ input: "#range_end" })]
+  });
 }
 
 export { initFlatpickr };
-
